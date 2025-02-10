1ef1aa42c198324529aa738ca4c3a1fd9e64fec8
Tu Nombre <tu-correo@ejemplo.com>
2025-02-10 15:52:43 -0500

    Descripción de los cambios

--- Obras/settings.py
+++ Obras/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'obraspublicas'  # Cambia esto a una clave secreta real
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['obrasdjango-2.onrender.com']  # Asegúrate de agregar tu dominio aquí
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -87,9 +87,6 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
